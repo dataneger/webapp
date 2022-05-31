031e32e6b761c61e23cc6343b1be3c56a49b1e76
Thomas van 't Wout <twvantwout@casema.nl>
2022-06-01 00:58:49 +0200

    Waldys eerst commit

--- public/index.php
+++ public/index.php
@@ -21,7 +21,7 @@
     <div class="container d-flex flex-wrap justify-content-center">
         <a href="/" class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none">
             <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-            <span class="fs-4">Double header</span>
+            <span class="fs-4">Waldys World</span>
         </a>
         <form class="col-12 col-lg-auto mb-3 mb-lg-0" role="search">
             <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
